6238c1c6ef8e25c9637a75d9843d548325822a87
Niels Nübel <n.nuebel@nn-medienagentur.de>
2019-01-11 10:37:02 +0100

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,23 +8,10 @@ git checkout gh-pages
 # build
 grunt fastBuild
 
-# navigate into the build output directory
+# Add Build Files
 git add -A
 git add -f index.html
 git add -f build/*
 git commit -m 'deploy'
 
 git push origin gh-pages
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
